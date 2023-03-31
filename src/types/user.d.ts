281cf36532d038730d69d842a318cf78f6186353
Sam Krallman <samuel.krallman@smail.astate.edu>
2023-03-31 02:27:25 -0500

    visit shortened link

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,4 +1,21 @@
+import { Relation } from 'typeorm/common/RelationType';
+import { Link } from '../entities/Link';
+
 type NewUserRequest = {
   username: string;
   password: string;
+};
+
+type NewLinkRequest = {
+  originalURL: string;
+};
+
+type LinkParam = {
+  linkId: string;
+};
+
+type UserParam = {
+  isPro: boolean;
+  isAdmin: boolean;
+  links: Relation<Link>[];
 };
